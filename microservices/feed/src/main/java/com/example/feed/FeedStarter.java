e88fca5eb653473452f7c8779bab0000a96bc95c
sergeynikiforov761 <nikiforov.sv@phystech.edu>
2020-02-26 18:10:49 +0300

    + Feed API

---
+++ microservices/feed/src/main/java/com/example/feed/FeedStarter.java
@@ -0,0 +1,12 @@
+package com.example.feed;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class FeedStarter {
+    public static void main(String[] args) {
+        run(FeedStarter.class, args);
+    }
+}
